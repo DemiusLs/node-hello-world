0e7bde8ff9ea68690047f7592dfc510a4067b049
DemiusLs <dloss320@gmail.com>
2025-05-21 15:01:27 +0200

    feat: bonus 1 plus

--- functions/functions.js
+++ functions/functions.js
@@ -3,16 +3,26 @@
 const findVocals = (string) => {
     const vocals = ["a", "e", "i", "o", "u"]
     let arr = string.split("")
-    let retArr = []
+    let vocalsCount = {
+        a: 0,
+        e: 0,
+        i: 0,
+        o: 0,
+        u: 0,
+    }
     arr.forEach(element => {
-        vocals.find(vocal => vocal === element.toLowerCase()) && retArr.push(element)  ;
+        vocals.find(vocal => vocal === element.toLowerCase()) && (vocalsCount[element.toLowerCase()]++);
 
         // Vorrei contare ogni singola vocale
 
     });
+    let count = 0;
+    for (let vocal in vocalsCount) {
+        count = count + vocalsCount[vocal]
+    }
+    
 
-    return retArr.length
-
+    return count 
 }
 
 export { findVocals };
